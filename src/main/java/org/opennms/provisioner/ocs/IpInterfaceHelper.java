359935673b756bdd9bcbbe5d970e1aeea9e8cb76
Markus Neumann <Markus@OpenNMS.org>
2013-09-26 13:34:19 +0200

    added some safty for handling iplike match patterns

--- src/main/java/org/opennms/provisioner/ocs/IpInterfaceHelper.java
+++ src/main/java/org/opennms/provisioner/ocs/IpInterfaceHelper.java
@@ -6,39 +6,48 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import org.opennms.core.utils.IPLike;
 import org.opennms.ocs.inventory.client.response.Computer;
 import org.opennms.ocs.inventory.client.response.Network;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class IpInterfaceHelper {
-    
+
     private static final Logger LOGGER = LoggerFactory.getLogger(IpInterfaceHelper.class);
-    
     private List<String> ipBlackList = new ArrayList<>();
     private List<String> ipWhiteList = new ArrayList<>();
-    
+
     public Network selectManagementNetworkWhiteAndBlackOnly(Computer computer) {
         List<Network> possibleNetworks = new ArrayList<>();
         for (Network network : computer.getNetworks()) {
-            if (isIpWhiteListed(network.getIPAddress()) && !isIpBlackListed(network.getIPAddress())) {
-                possibleNetworks.add(network);
+            if (!network.getIPAddress().isEmpty()) {
+                if (isIpWhiteListed(network.getIPAddress()) && !isIpBlackListed(network.getIPAddress())) {
+                    possibleNetworks.add(network);
+                }
+            } else {
+                LOGGER.debug("FOUND A NETWORK WITHOUT IPADDRESS FOR COMPUTER ID:{} NAME:{}", computer.getHardware().getId(), computer.getHardware().getName());
             }
         }
         if (possibleNetworks.size() >= 1) {
             return possibleNetworks.get(0);
         } else {
             String ocsPickedIp = computer.getHardware().getIpaddr();
-            if(isIpWhiteListed(ocsPickedIp) && !isIpBlackListed(ocsPickedIp)) {
-                Network fakeNetwork = new Network();
-                fakeNetwork.setIPAddress(ocsPickedIp);
-                return fakeNetwork;
+            if (!ocsPickedIp.isEmpty()) {
+                if (isIpWhiteListed(ocsPickedIp) && !isIpBlackListed(ocsPickedIp)) {
+                    Network fakeNetwork = new Network();
+                    fakeNetwork.setIPAddress(ocsPickedIp);
+                    return fakeNetwork;
+                } else {
+                    return null;
+                }
             } else {
+                LOGGER.debug("FALL BACK TO OCS PICKED IP BUT IP WAS EMPTY FOR COMPUTER ID:{} NAME:{}", computer.getHardware().getId(), computer.getHardware().getName());
                 return null;
             }
         }
     }
-    
+
     public Network selectManagementNetwork(Computer computer) {
 
         List<Network> possibleNetworks = new ArrayList<>();
@@ -70,8 +79,11 @@ public class IpInterfaceHelper {
 
     private Boolean isIpBlackListed(String ipAddress) {
         for (String blackedIp : ipBlackList) {
-            if (ipAddress.startsWith(blackedIp)) {
+            if (IPLike.matches(ipAddress, blackedIp)) {
+                LOGGER.debug("IpAddress Black: {} \t vs \t {} \t OK", ipAddress, blackedIp);
                 return true;
+            } else {
+                LOGGER.debug("IpAddress Black: {} \t vs \t {} \t no", ipAddress, blackedIp);
             }
         }
         return false;
@@ -79,30 +91,51 @@ public class IpInterfaceHelper {
 
     private Boolean isIpWhiteListed(String ipAddress) {
         for (String whiteIp : ipWhiteList) {
-            if (ipAddress.startsWith(whiteIp)) {
+            if (IPLike.matches(ipAddress, whiteIp)) {
+                LOGGER.debug("IpAddress White: {} \t vs \t {} \t OK", ipAddress, whiteIp);
                 return true;
+            } else {
+                LOGGER.debug("IpAddress White: {} \t vs \t {} \t no", ipAddress, whiteIp);
             }
         }
         return false;
     }
-    
+
     public void addIpWhite(String ip) {
-        ipWhiteList.add(ip);
+        try {
+            IPLike.matches("1.1.1.1", ip);
+            ipWhiteList.add(ip);
+        } catch (Exception ex) {
+            LOGGER.error("WhiteList rejected illegal entry {}", ip, ex);
+        }
     }
-    
+
     public void addIpBlack(String ip) {
-        ipBlackList.add(ip);
+        try {
+            IPLike.matches("1.1.1.1", ip);
+            ipBlackList.add(ip);
+        } catch (Exception ex) {
+            LOGGER.error("BlackList rejected illegal entry {}", ip, ex);
+        }
     }
-    
+
     public void initListsFromConfigs() {
         //TODO make the black- and white-lists configurable in a nice way
         try {
-            ipBlackList = Files.readAllLines(Paths.get("./", "blackList.properties"), Charset.forName("UTF-8"));
-            ipWhiteList = Files.readAllLines(Paths.get("./", "whiteList.properties"), Charset.forName("UTF-8"));
+            List<String> rawBlackedList = Files.readAllLines(Paths.get("./", "blackList.properties"), Charset.forName("UTF-8"));
+            for (String rawBlacked : rawBlackedList) {
+                addIpBlack(rawBlacked);
+            }
+
+            List<String> rawWhiteList = Files.readAllLines(Paths.get("./", "whiteList.properties"), Charset.forName("UTF-8"));
+            for (String rawWhite : rawWhiteList) {
+                addIpWhite(rawWhite);
+            }
+
         } catch (IOException ex) {
             LOGGER.error("blackList and or whiteList could not be read from files, using empty lists.", ex);
             ipBlackList = new ArrayList<>();
             ipWhiteList = new ArrayList<>();
-        } 
+        }
     }
 }
