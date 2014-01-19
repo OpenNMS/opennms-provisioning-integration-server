276158b8fb079fc7ebd55e2d6937d36c45fd7076
Markus Neumann <Markus@OpenNMS.org>
2014-01-19 13:01:13 +0100

    Now HttpRequisitionSource is reading parameters from requisition.properties.

--- src/main/java/org/opennms/provisioner/source/HttpRequisitionSource.java
+++ src/main/java/org/opennms/provisioner/source/HttpRequisitionSource.java
@@ -33,51 +33,60 @@ public class HttpRequisitionSource implements Source {
     @Override
     public Object dump() throws Exception {
         LOGGER.info("HttpRequisitionSource at work....");
-
-        String requisitionUrl = "http://demo.opennms.com/opennms/rest/requisitions/Latency";
-
-        HttpClientBuilder builder = HttpClientBuilder.create();
-
-        // Get the client credentials
-        String username = "demo";
-        String password = "demo";
-
-        // If username and password was found, inject the credentials
-        if (username != null && password != null) {
-
-            CredentialsProvider provider = new BasicCredentialsProvider();
-
-            // Create the authentication scope
-            AuthScope scope = new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT, AuthScope.ANY_REALM);
-
-            // Create credential pair
-            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
-
-            // Inject the credentials
-            provider.setCredentials(scope, credentials);
-
-            // Set the default credentials provider
-            builder.setDefaultCredentialsProvider(provider);
-        }
-        HttpClient client = builder.build();
-
-        HttpGet request = new HttpGet(requisitionUrl);
-        HttpResponse response = client.execute(request);
         Requisition requisition = null;
-        try {
 
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            JAXBContext jaxbContext = JAXBContext.newInstance(Requisition.class);
+        if (getUrl() != null) {
+            HttpClientBuilder builder = HttpClientBuilder.create();
 
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            requisition = (Requisition) jaxbUnmarshaller.unmarshal(bufferedReader);
+            // If username and password was found, inject the credentials
+            if (getUserName() != null && getPassword() != null) {
 
-        } catch (JAXBException e) {
-            LOGGER.error("The responce did not contain a valid requisition as xml.", e);
+                CredentialsProvider provider = new BasicCredentialsProvider();
+
+                // Create the authentication scope
+                AuthScope scope = new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT, AuthScope.ANY_REALM);
+
+                // Create credential pair
+                UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(getUserName(), getPassword());
+
+                // Inject the credentials
+                provider.setCredentials(scope, credentials);
+
+                // Set the default credentials provider
+                builder.setDefaultCredentialsProvider(provider);
+            }
+            HttpClient client = builder.build();
+
+            HttpGet request = new HttpGet(getUrl());
+            HttpResponse response = client.execute(request);
+            try {
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                JAXBContext jaxbContext = JAXBContext.newInstance(Requisition.class);
+
+                Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+                requisition = (Requisition) jaxbUnmarshaller.unmarshal(bufferedReader);
+
+            } catch (JAXBException e) {
+                LOGGER.error("The responce did not contain a valid requisition as xml.", e);
+            }
+            LOGGER.debug("Got Requisition {}", requisition);
+        } else {
+            LOGGER.error("Parameter requisition.url is missing in requisition.properties");
         }
-        LOGGER.debug("Got Requisition {}", requisition);
-
         return requisition;
+    }
+
+    public final String getUserName() {
+        return this.config.getString("requisition.username");
+    }
+
+    public final String getPassword() {
+        return this.config.getString("requisition.password");
+    }
+
+    public final String getUrl() {
+        return this.config.getString("requisition.url");
     }
 
     public static class Factory implements Source.Factory {
