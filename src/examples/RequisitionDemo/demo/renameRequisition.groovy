6d090ae5d592c676a7b071247f2d4a903c345f4a
Markus Neumann <Markus@OpenNMS.org>
2014-01-20 14:59:37 +0100

    sample to add locations via custom script

--- src/examples/RequisitionDemo/demo/renameRequisition.groovy
+++ src/examples/RequisitionDemo/demo/renameRequisition.groovy
@@ -1,11 +1,36 @@
 import org.opennms.netmgt.provision.persist.requisition.Requisition
+import org.opennms.netmgt.provision.persist.requisition.RequisitionAsset
 import org.opennms.netmgt.provision.persist.requisition.RequisitionNode
+import org.opennms.netmgt.provision.persist.requisition.RequisitionCategory
+
+final String ASSET_LATITUDE = "latitude"
+final String ASSET_LONGITUDE = "longitude"
 
 Requisition requisition = data
 requisition.setForeignSource(instance)
 for (RequisitionNode node : requisition.getNodes()) {
-    node.setBuilding(instance)
+    node.setBuilding("")
 }
+
+for (RequisitionNode node : requisition.getNodes()) {
+    for (RequisitionCategory category : node.getCategories()) {
+        
+        if (category.getName().equals("USA")) {
+            node.getAssets().add(new RequisitionAsset(ASSET_LATITUDE, "35.71736"))
+            node.getAssets().add(new RequisitionAsset(ASSET_LONGITUDE, "-79.16181"))
+        }
+        if (category.getName().equals("GERMANY")) {
+            node.getAssets().add(new RequisitionAsset(ASSET_LATITUDE, "50.555451"))
+            node.getAssets().add(new RequisitionAsset(ASSET_LONGITUDE, "9.678587"))
+        }
+        if (category.getName().equals("ITALY")) {
+            node.getAssets().add(new RequisitionAsset(ASSET_LATITUDE, "46.894884"))
+            node.getAssets().add(new RequisitionAsset(ASSET_LONGITUDE, "11.435673"))
+        }
+    }
+}
+
 logger.info("Requisition: {}", requisition)
 
 return requisition
+
