0a2500f203fdf570d43953b86d6298901f91d177
Markus Neumann <Markus@OpenNMS.org>
2014-01-25 19:35:50 +0100

    address columns via header-row prefix lookup

--- src/main/java/org/opennms/provisioner/xls/source/XlsSource.java
+++ src/main/java/org/opennms/provisioner/xls/source/XlsSource.java
@@ -2,6 +2,9 @@ package org.opennms.provisioner.xls.source;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
+import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
@@ -20,10 +23,20 @@ import org.slf4j.LoggerFactory;
 public class XlsSource implements Source {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(XlsSource.class);
-    private final String ASSET_DESCRIPTION = "description";
     private final String instance;
     private final Configuration config;
+    private final String SPLITTER = ",";
 
+    private final String PREFIX_NODE = "Node_";
+    private final String PREFIX_CATEGORY = "cat_";
+    private final String PREFIX_SERVICE = "svc_";
+    private final String PREFIX_IP_ADDRESS = "IP_";
+    private final String PREFIX_INTERFACE_TYPE = "IfType_";
+    private final String PREFIX_ASSET_DESCRIPTION = "Asset_Description";
+
+    private final String INTERFACE_TYPE_PRIMARY = "P";
+    private final String INTERFACE_TYPE_SECONDARY = "S";
+    private final String ASSET_DESCRIPTION = "description";
 
     public static class Factory implements Source.Factory {
 
@@ -55,39 +68,25 @@ public class XlsSource implements Source {
                     RequisitionInterface reqInterface;
                     Integer row = 1;
                     while (row < sheet.getRows()) {
+                        //TODO clean this if
                         if (row.equals(1) || !sheet.getCell(0, row).getContents().trim().equalsIgnoreCase(sheet.getCell(0, row - 1).getContents().trim())) {
+                            String nodeLabel = sheet.getCell(getRelevantColumnID(sheet, PREFIX_NODE), row).getContents().trim();
                             node = new RequisitionNode();
-                            node.setNodeLabel(sheet.getCell(0, row).getContents().trim());
-                            node.setForeignId(sheet.getCell(0, row).getContents().trim());
-                            if (!sheet.getCell(3, row).getContents().isEmpty()) {
-                                node.getAssets().add(new RequisitionAsset(ASSET_DESCRIPTION, sheet.getCell(3, row).getContents().trim()));
+                            node.setNodeLabel(nodeLabel);
+                            node.setForeignId(nodeLabel);
+                            String assetDescription = sheet.getCell(getRelevantColumnID(sheet, PREFIX_ASSET_DESCRIPTION), row).getContents().trim();
+                            if (!assetDescription.isEmpty()) {
+                                node.getAssets().add(new RequisitionAsset(ASSET_DESCRIPTION, assetDescription));
                             }
-
-                            Integer column = 5;
-                            while (column < sheet.getRow(row).length) {
-                                if (!sheet.getCell(column, row).getContents().isEmpty()) {
-                                    node.getCategories().add(new RequisitionCategory(sheet.getCell(column, row).getContents().trim()));
-                                }
-                                column++;
-                            }
+                            //adding categories
+                            node.getCategories().addAll(getCategoriesByRow(sheet, row));
                             requisition.getNodes().add(node);
                         }
-                        reqInterface = new RequisitionInterface();
-                        reqInterface.setIpAddr(sheet.getCell(1, row).getContents());
-                        if (sheet.getCell(2, row).getContents().equalsIgnoreCase("P")) {
-                            reqInterface.setSnmpPrimary(PrimaryType.PRIMARY);
-                        } else if (sheet.getCell(2, row).getContents().equalsIgnoreCase("S")) {
-                            reqInterface.setSnmpPrimary(PrimaryType.SECONDARY);
-                        } else {
-                            reqInterface.setSnmpPrimary(PrimaryType.NOT_ELIGIBLE);
-                        }
+                        //Add interface
+                        reqInterface = getInterfaceByRow(sheet, row);
+
                         //Add services to the interface
-                        if (!sheet.getCell(4, row).getContents().isEmpty()) {
-                            String[] forcedServices = sheet.getCell(4, row).getContents().trim().split(",");
-                            for (String service : forcedServices) {
-                                reqInterface.getMonitoredServices().add(new RequisitionMonitoredService(service.trim()));
-                            }
-                        }
+                        reqInterface.getMonitoredServices().addAll(getServicesByRow(sheet, row));
                         node.getInterfaces().add(reqInterface);
                         row++;
                     }
@@ -103,6 +102,73 @@ public class XlsSource implements Source {
             throw new RuntimeException("no xls.file defined");
         }
         return requisition;
+    }
+
+    //TODO null protection
+    private Integer getRelevantColumnID(Sheet sheet, String prefix) {
+        Cell[] row = sheet.getRow(0);
+        for (Cell cell : row) {
+            if (cell.getContents().trim().toLowerCase().startsWith(prefix.toLowerCase())) {
+                return (cell.getColumn());
+            }
+        }
+        LOGGER.debug("NO Column in {} starting with {}", sheet.getName(), prefix);
+        return null;
+    }
+
+    private Set<Integer> getRelevantColumnIDs(Sheet sheet, String prefix) {
+        Set<Integer> relevantColumnIDs = new TreeSet<>();
+        Cell[] row = sheet.getRow(0);
+        for (Cell cell : row) {
+            if (cell.getContents().trim().toLowerCase().startsWith(prefix.toLowerCase())) {
+                relevantColumnIDs.add(cell.getColumn());
+            }
+        }
+        LOGGER.debug("Found {} Columns for {} starting with {}", relevantColumnIDs.size(), sheet.getName(), prefix);
+        return relevantColumnIDs;
+    }
+
+    private Set<RequisitionCategory> getCategoriesByRow(Sheet sheet, Integer rowID) {
+        Set<RequisitionCategory> categories = new TreeSet<>();
+        for (Integer column : getRelevantColumnIDs(sheet, PREFIX_CATEGORY)) {
+            String rawCategories = sheet.getCell(column, rowID).getContents().trim();
+            for (String category : rawCategories.split(SPLITTER)) {
+                category = category.trim();
+                if (!category.isEmpty()) {
+                    categories.add(new RequisitionCategory(category));
+                }
+            }
+        }
+        return categories;
+    }
+
+    private Set<RequisitionMonitoredService> getServicesByRow(Sheet sheet, Integer rowID) {
+        Set<RequisitionMonitoredService> services = new TreeSet<>();
+        for (Integer column : getRelevantColumnIDs(sheet, PREFIX_SERVICE)) {
+            String rawServices = sheet.getCell(column, rowID).getContents().trim();
+            for (String service : rawServices.split(SPLITTER)) {
+                service = service.trim();
+                if (!service.isEmpty()) {
+                    services.add(new RequisitionMonitoredService(service));
+                }
+            }
+        }
+        return services;
+    }
+
+    private RequisitionInterface getInterfaceByRow(Sheet sheet, Integer rowID) {
+        RequisitionInterface reqInterface = new RequisitionInterface();
+
+        reqInterface.setIpAddr(sheet.getCell(getRelevantColumnID(sheet, PREFIX_IP_ADDRESS), rowID).getContents().trim());
+        String interfaceType = sheet.getCell(getRelevantColumnID(sheet, PREFIX_INTERFACE_TYPE), rowID).getContents().trim();
+        if (interfaceType.equalsIgnoreCase(INTERFACE_TYPE_PRIMARY)) {
+            reqInterface.setSnmpPrimary(PrimaryType.PRIMARY);
+        } else if (interfaceType.equalsIgnoreCase(INTERFACE_TYPE_SECONDARY)) {
+            reqInterface.setSnmpPrimary(PrimaryType.SECONDARY);
+        } else {
+            reqInterface.setSnmpPrimary(PrimaryType.NOT_ELIGIBLE);
+        }
+        return reqInterface;
     }
 
     public String getXlsFile() {
