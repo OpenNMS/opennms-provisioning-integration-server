099db379ad38b9e4229a384e1c74aab9169febd3
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2014-01-28 21:46:28 +0100

    Added Jdbc

--- src/main/java/org/opennms/provisioner/jdbc/source/JdbcSource.java
+++ src/main/java/org/opennms/provisioner/jdbc/source/JdbcSource.java
@@ -1,17 +1,26 @@
 package org.opennms.provisioner.jdbc.source;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import org.apache.commons.configuration.Configuration;
+import org.apache.commons.lang.StringUtils;
+import org.opennms.netmgt.model.PrimaryType;
 import org.opennms.netmgt.provision.persist.requisition.Requisition;
+import org.opennms.netmgt.provision.persist.requisition.RequisitionAsset;
+import org.opennms.netmgt.provision.persist.requisition.RequisitionCategory;
 import org.opennms.netmgt.provision.persist.requisition.RequisitionInterface;
+import org.opennms.netmgt.provision.persist.requisition.RequisitionMonitoredService;
 import org.opennms.netmgt.provision.persist.requisition.RequisitionNode;
 import org.opennms.provisioner.source.Source;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class JdbcSource implements Source {
 
@@ -82,25 +91,91 @@ public class JdbcSource implements Source {
 
         try {
             statement = connection.createStatement();
+
+            LOGGER.info(this.getJdbcSelectStatement());
+
+            Set<String> columns = new TreeSet<String>();
+
             resultSet = statement.executeQuery(this.getJdbcSelectStatement());
+
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+            for (int i = 0; i < resultSetMetaData.getColumnCount(); i++) {
+                columns.add(resultSetMetaData.getColumnName(i + 1));
+            }
+
+            LOGGER.info("Columns: {}",columns);
 
             if (resultSet != null) {
                 while (resultSet.next()) {
-                    String nodeLabel = resultSet.getString("NodeLabel").trim();
-                    LOGGER.info("NodeLabel :: {}", nodeLabel);
+                    String nodeLabel = getString(resultSet, "Node_Label", columns);
 
-                    RequisitionNode node = new RequisitionNode();
-                    node.setNodeLabel(nodeLabel);
-                    node.setForeignId(nodeLabel);
+                    if (nodeLabel == null) {
+                        LOGGER.warn("Node_Label is null");
+                        continue;
+                    }
 
-                    String ipAddress = resultSet.getString("IpAddress").trim();
-                    RequisitionInterface reqInterface = new RequisitionInterface();
-                    reqInterface.setIpAddr(ipAddress);
+                    RequisitionNode node = requisition.getNode(nodeLabel);
 
-                    requisition.getNodes().add(node);
+                    if (node == null) {
+                        node = new RequisitionNode();
+                        node.setNodeLabel(nodeLabel);
+                        node.setForeignId(nodeLabel);
+                        requisition.getNodes().add(node);
+                    } else {
+                        LOGGER.info("Existing node - updating {} ", nodeLabel);
+                    }
+
+
+                    String description = getString(resultSet, "Asset_Description", columns);
+
+                    if (description != null) {
+                        node.getAssets().add(new RequisitionAsset("description", description));
+                    }
+
+                    String ipAddress = getString(resultSet, "Ip_Address", columns);
+
+                    if (ipAddress != null) {
+                        RequisitionInterface reqInterface = node.getInterface(ipAddress);
+
+                        if (reqInterface == null) {
+                            reqInterface = new RequisitionInterface();
+                            reqInterface.setIpAddr(ipAddress);
+                            node.getInterfaces().add(reqInterface);
+                        }
+
+                        String ifType = getString(resultSet, "If_Type", columns);
+
+                        if ("P".equalsIgnoreCase(ifType)) {
+                            reqInterface.setSnmpPrimary(PrimaryType.PRIMARY);
+                        } else {
+                            if ("S".equalsIgnoreCase(ifType)) {
+                                reqInterface.setSnmpPrimary(PrimaryType.SECONDARY);
+                            } else {
+                                reqInterface.setSnmpPrimary(PrimaryType.NOT_ELIGIBLE);
+                            }
+                        }
+                        String service = getString(resultSet, "Svc", columns);
+
+                        if (service != null) {
+                            if (reqInterface.getMonitoredService(service) == null) {
+                                reqInterface.getMonitoredServices().add(new RequisitionMonitoredService(service));
+                            }
+                        }
+                    } else {
+                        LOGGER.warn("Ip_Address is null, ignoring If_Type and Svc");
+                    }
+
+                    String category = getString(resultSet, "Cat", columns);
+
+                    if (category != null) {
+                        if (node.getCategory(category) == null) {
+                            node.getCategories().add(new RequisitionCategory(category));
+                        }
+                    }
                 }
             } else {
-                LOGGER.error("ResultSet was NULL");
+                LOGGER.error("ResultSet is null");
             }
 
         } catch (SQLException ex) {
@@ -108,6 +183,24 @@ public class JdbcSource implements Source {
         }
 
         return requisition;
+    }
+
+    private String getString(ResultSet resultSet, String columnName, Set<String> columns) {
+        if (!columns.contains(columnName.toLowerCase())) {
+            return null;
+        }
+
+        try {
+            String result = resultSet.getString(columnName);
+
+            if (result != null) {
+                return result.trim();
+            }
+
+            return result;
+        } catch (SQLException sqlException) {
+            return null;
+        }
     }
 
     public String getJdbcDriver() {
@@ -127,6 +220,7 @@ public class JdbcSource implements Source {
     }
 
     public String getJdbcSelectStatement() {
-        return this.config.getString("jdbc.selectStatement", null);
+        String arr[] = this.config.getStringArray("jdbc.selectStatement");
+        return StringUtils.join(arr, ',');
     }
 }
