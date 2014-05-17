cb0d134ae204e501d35be7434491890b4bfcd2e5
Markus Neumann <Markus@OpenNMS.org>
2014-05-17 16:57:09 +0200

    Improved reverseDNS.groovy to stop checking for DNS names as soon as one valid was found for a node.

--- opennms-pris/src/examples/scripts/reverseDNS.groovy
+++ opennms-pris/src/examples/scripts/reverseDNS.groovy
@@ -5,18 +5,16 @@ import org.opennms.netmgt.provision.persist.requisition.RequisitionInterface
 logger.info("starting reverseDNS.groovy")
 
 for (RequisitionNode node : requisition.getNodes()) {
-
   for (RequisitionInterface myInterface : node.getInterfaces()) {
     String ipAddress = myInterface.getIpAddr()
     String dnsNodeLabel = InetAddress.getByName(ipAddress).getCanonicalHostName()
     logger.debug("For foreignID '{}' dnsNodeLabel for IP '{}' is '{}'", node.getForeignId(), ipAddress, dnsNodeLabel)
-    if (ipAddress.equals(dnsNodeLabel)) {
-      logger.debug("No dns-name was found for node '{}' and ip '{}' using ip as nodeLabel", node.getForeignId(), ipAddress)
+    if (!ipAddress.equals(dnsNodeLabel)) {
+      logger.info("Using '{}' as NodeLabel for foreignId '{}' based on IP '{}'", dnsNodeLabel, node.getForeignId(), ipAddress)
+      node.setNodeLabel(dnsNodeLabel)
+      break
     }
-    node.setNodeLabel(dnsNodeLabel)
   }
 }
-
 logger.info("done with reverseDNS.groovy")
-
 return requisition
