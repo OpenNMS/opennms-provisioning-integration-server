4ce00a70b199545318b05b22b3c588ef4a30438a
Markus Neumann <Markus@OpenNMS.org>
2014-06-18 19:49:32 +0200

    http://ip:port/documentation/ now serves the documentation

--- opennms-pris-main/src/main/java/org/opennms/pris/driver/HttpServerDriver.java
+++ opennms-pris-main/src/main/java/org/opennms/pris/driver/HttpServerDriver.java
@@ -25,19 +25,12 @@
  */
 package org.opennms.pris.driver;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.opennms.pris.RequisitionGenerator;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.opennms.pris.api.Configuration;
-import org.opennms.pris.model.Requisition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -75,73 +68,25 @@ public class HttpServerDriver implements Driver {
         final Server server = new Server(new InetSocketAddress(this.config.getString("host", "127.0.0.1"),
                 this.config.getInt("port", 8686)));
 
-        // Create the marshaller for the requisition
-        final JAXBContext jaxbContext = JAXBContext.newInstance(Requisition.class);
+        
+        ContextHandlerCollection contextHandlerCollection = new ContextHandlerCollection();
+        
+        RequisitionProviderHandler requisitionProviderHandler = new RequisitionProviderHandler();
+        ContextHandler contextHandlerRequisitions = new ContextHandler("/requisitions");
+        contextHandlerRequisitions.setHandler(requisitionProviderHandler);
+        contextHandlerCollection.addHandler(contextHandlerRequisitions);
+        
+        ResourceHandler docuResourceHandler = new ResourceHandler();
+        docuResourceHandler.setDirectoriesListed(true);
+        docuResourceHandler.setWelcomeFiles(new String[]{"index.html"});
+        docuResourceHandler.setResourceBase("./documentation/");
+        ContextHandler contextHandlerDocu = new ContextHandler("/documentation");
+        contextHandlerDocu.setHandler(docuResourceHandler);
+        contextHandlerCollection.addHandler(contextHandlerDocu);
 
-        final Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        server.setHandler(contextHandlerCollection);
 
-        // Create a handler for the HTTP requests returning a requisition
-        server.setHandler(new AbstractHandler() {
-
-            @Override
-            public void handle(final String target,
-                    final Request baseRequest,
-                    final HttpServletRequest request,
-                    final HttpServletResponse response) throws IOException,
-                    ServletException {
-                // As this is the only handler, we mark every request as handled
-                baseRequest.setHandled(true);
-
-                String[] pathParts = request.getPathInfo().substring(1).split("/");
-
-                //Check if a requisition or something else was requested
-                final String requestType = pathParts[0];
-                LOGGER.debug("requestType is '{}'", requestType);
-
-                if (requestType != null) {
-                    if (requestType.equalsIgnoreCase("requisitions")) {
-
-                        // Get the instance for the request path
-                        String instance = null;
-                        if (pathParts.length > 1) {
-                            instance = pathParts[1];
-                        }
-
-                        if (instance == null || instance.isEmpty() || instance.contains("favicon.ico")) {
-                            response.sendError(404, "No instance specified");
-                        } else {
-                            LOGGER.debug("Handling request for instance: {}", instance);
-                            try {
-                                // Create the requisition provider for the instance
-                                final RequisitionGenerator requisitionProvider = new RequisitionGenerator(instance);
-
-                                // Generate the requisition
-                                final Requisition requisition = requisitionProvider.generate(instance);
-
-                                // Marshall the requisition and write it to the response stream
-                                jaxbMarshaller.marshal(requisition, response.getOutputStream());
-                            } catch (final Exception ex) {
-                                response.sendError(500, ex.getMessage());
-                                LOGGER.warn("Request failed", ex);
-                            }
-                        }
-                    } else {
-                        if (requestType.equalsIgnoreCase("documentation")) {
-                            response.sendError(500, "documentations is not supported at the moment");
-                        } else {
-                            response.sendError(500, "requestType " + requestType + " causes a problem. try requisitions/myInstance");
-                            LOGGER.error("requestType '{}' of path '{}'", requestType, request.getPathInfo());
-                        }
-                    }
-                } else {
-                    response.sendError(404, "No requisitions/instance specified");
-                }
-            }
-        });
-        // Start the server
         server.start();
-
         server.join();
     }
 }
