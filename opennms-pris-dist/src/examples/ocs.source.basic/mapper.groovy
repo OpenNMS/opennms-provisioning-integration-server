4fdbf83e86f8f8a4984187007bcd63f317d2acc9
Markus Neumann <Markus@OpenNMS.org>
2014-06-18 13:32:13 +0200

    updated ocs examples

--- opennms-pris-dist/src/examples/ocs.source.basic/mapper.groovy
+++ opennms-pris-dist/src/examples/ocs.source.basic/mapper.groovy
@@ -4,42 +4,21 @@ import org.opennms.pris.model.RequisitionNode
 import org.opennms.ocs.inventory.client.response.Computer;
 import org.opennms.ocs.inventory.client.response.Computers;
 import org.opennms.ocs.inventory.client.response.Drive;
+import org.opennms.pris.util.RequisitionUtils;
 
+// This sample script for ocs computers overwrites the foreignId to be the ocs systemId / hardwareId
 final Computers myComputers = data;
 Requisition myRequisition = requisition;
 
 for (Computer computer : myComputers.getComputers()) {
     logger.debug("Processing Computer {}", computer.getHardware().getName());
 
-    RequisitionNode node = myRequisition.getNode(computer.getHardware().getName());
+    RequisitionNode node = RequisitionUtils.findNode(myRequisition, computer.getHardware().getName());
     logger.debug("Processing node {}", node);
- 
+
     if (node != null) {
         logger.debug("looking for drives")
-        
-        //map more stuff
-        int i = 1
-        for (Drive drive : computer.getDrives()) {
-            
-            logger.debug("drive '{}'", drive)
-            String hddAsset = ""
-            
-            // Windows
-            if (drive.getType() == "Hard Drive") {
-                hddAsset = drive.getLetter() + " is " + drive.getFilesystem() + " as " + drive.getVolumn() + " " + drive.getFree() + "/" + drive.getTotal()
-            }
-            
-            //Linxu/Unix
-            if (drive.getType().startsWith("/")) {
-                hddAsset = drive.getType() + " is " + drive.getFilesystem() + " " + drive.getFree() + "/" + drive.getTotal()
-            }
-            logger.debug("Node '{}' \t hddAsset '{}'", node.getNodeLabel(), hddAsset)
-            
-            if (i <= 6 && !hddAsset.isEmpty()) {
-                node.getAssets().add(new RequisitionAsset("hdd" +i, hddAsset))
-            }
-            i++
-        }
+        node.setForeignId(computer.getHardware().getId());
     }
 }
 
