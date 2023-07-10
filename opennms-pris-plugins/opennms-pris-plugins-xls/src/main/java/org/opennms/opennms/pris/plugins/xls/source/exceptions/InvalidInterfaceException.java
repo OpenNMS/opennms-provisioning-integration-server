afb4ee08081b8d37c9766a92b7893959ebb47c3f
Benjamin Reed <ranger@opennms.org>
2023-07-10 14:01:26 -0400

    first submission of javadocs

--- opennms-pris-plugins/opennms-pris-plugins-xls/src/main/java/org/opennms/opennms/pris/plugins/xls/source/exceptions/InvalidInterfaceException.java
+++
@@ -1,8 +0,0 @@
-package org.opennms.opennms.pris.plugins.xls.source.exceptions;
-
-public class InvalidInterfaceException extends Exception {
-
-    public InvalidInterfaceException(String string, IllegalArgumentException ex) {
-        super(string, ex);
-    }
-}
